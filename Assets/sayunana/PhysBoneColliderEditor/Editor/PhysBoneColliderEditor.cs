e7c017a72d128b535de43f8bbe28e2fb13e42590
sayunana <sayunanasan@gmail.com>
2025-01-05 18:09:55 +0900

    Init

---
+++ Assets/sayunana/PhysBoneColliderEditor/Editor/PhysBoneColliderEditor.cs
@@ -0,0 +1,97 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+
+namespace sayunana
+{
+    public class PhysBoneColliderEditor : EditorWindow
+    {
+        private Editor _editor;
+
+        [SerializeField] private List<VRCPhysBone> _vrcPhysBoneList = new List<VRCPhysBone>();
+
+        [SerializeField] private List<VRCPhysBoneCollider> _vrcPhysBoneColliderList = new List<VRCPhysBoneCollider>();
+
+        [MenuItem("sayunana/PhysBoneCollider Set Remover")]
+        static void Open()
+        {
+            var window = GetWindow<PhysBoneColliderEditor>();
+            window.titleContent = new GUIContent("PhysBoneCollider Set Remover");
+        }
+
+        private void OnGUI()
+        {
+            GUIStyle style = new GUIStyle(GUI.skin.label);
+            style.wordWrap = true;
+            GUILayout.Label("「VRCPhysBoneSelectList」に登録した「VRCPhysBone」に対して、「VRCPhysBoneColliderSelectList」に登録した「VRCPhysBoneCollider」を追加または削除をします",style);
+            
+            var so = new SerializedObject(this);
+
+            so.Update();
+
+            EditorGUILayout.PropertyField(so.FindProperty("_vrcPhysBoneList"), new GUIContent("VRCPhysBoneSelectList"), true);
+
+            EditorGUILayout.PropertyField(so.FindProperty("_vrcPhysBoneColliderList"),
+                new GUIContent("VRCPhysBoneColliderSelectList"), true);
+
+            so.ApplyModifiedProperties();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                if (GUILayout.Button("Add"))
+                {
+                    if (IsCheckList())
+                    {
+                        _vrcPhysBoneList.ForEach(vrcPhysBone =>
+                        {
+                            _vrcPhysBoneColliderList.ForEach(vrcPhysBoneCollider =>
+                            {
+                                if (!vrcPhysBone.colliders.Contains(vrcPhysBoneCollider))
+                                {
+                                    vrcPhysBone.colliders.Add(vrcPhysBoneCollider);
+                                }
+                            });
+                        });
+                    }
+                }
+
+                if (GUILayout.Button("Remove"))
+                {
+                    if (IsCheckList())
+                    {
+                        _vrcPhysBoneList.ForEach(vrcPhysBone =>
+                        {
+                            _vrcPhysBoneColliderList.ForEach(vrcPhysBoneCollider =>
+                            {
+                                if (vrcPhysBone.colliders.Contains(vrcPhysBoneCollider))
+                                {
+                                    vrcPhysBone.colliders.Remove(vrcPhysBoneCollider);
+                                }
+                            });
+                        });
+                    }
+                }
+            }
+        }
+
+        private bool IsCheckList()
+        {
+            bool isCheck = true;
+            if (_vrcPhysBoneList.Count == 0)
+            {
+                Debug.LogError("VRCPhysBoneSelectListに登録がありません");
+                isCheck = false;
+            }
+
+            if (_vrcPhysBoneColliderList.Count == 0)
+            {
+                Debug.LogError("VRCPhysBoneColliderSelectListに登録がありません");
+                isCheck = false;
+            }
+            return isCheck;
+        }
+    }
+}
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PhysBoneColliderSetRemover
+VRCPhysBoneのColliderをまとめて追加または削除します。
+
+# 使い方
+メニューバーから `sayunana` -> `PhysBoneCollider Set Remover`でエディターを開きます。
+
+
+# サポート環境
+◯Unity 2022.3.22f1\
+△Unity 2019.4.31f1は現在動作未確認です。
+
+# UPM
+`https://github.com/sayunana/PhysBoneColliderSetRemover.git?path=Assets/sayunana`をUnity Package Managerの`Add package from git url`で追加してください。
+
+# VCC
+[ここ](https://sayunana.github.io/vpm-repos/)から登録できます。
